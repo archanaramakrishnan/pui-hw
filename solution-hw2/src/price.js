4505a046f57589fd35bb80a6dedf2d0690917bd7
Archana Ramakrishnan <archana2359@gmail.com>
2022-09-15 15:21:43 -0400

    Dynamic drop down for first roll

---
+++ solution-hw2/src/price.js
@@ -0,0 +1,57 @@
+
+const glazingWithPrice = { 
+    "Keep original" : 0.00,
+    "Sugar milk" : 0.00,
+    "Vanilla milk" : 0.50,
+    "Double chocolate" : 1.50
+};
+
+const roll = { 
+    "Original" : 2.49,
+    "Apple" : 3.49,
+    "Raisin" : 2.99,
+    "Walnut" : 3.49,
+    "Double-chocolate" : 3.99,
+    "Strawberry" : 3.99,
+};
+
+// type, price, glazing and packSize
+
+
+function populateGlazingOptions() {
+    let glazingDropDown = document.getElementById("glazing-options");
+    
+    for (const glazing in glazingWithPrice)
+    {
+        let option = document.createElement("option");
+        glazingDropDown.add(option);
+        option.text = glazing;
+    }
+}
+
+
+// function populateGlazingOptions() {
+//     let glazingDropDown = document.getElementsByClassName("glazing-options");
+//     console.log(glazingDropDown)
+
+//     for (const dropDown in glazingDropDown)
+//     {
+//         console.log(dropDown[0])
+//         for (const glazing in glazingWithPrice)
+//         {
+//             let option = document.createElement("option");
+//             dropDown.add(option);
+//             option.text = glazing;
+//         }
+//     }
+    
+    
+// }
+
+
+function calculatePrice() {
+    // (basePrice + glazingPrice) * packPrice
+}
+
+calculatePrice();
+populateGlazingOptions();
